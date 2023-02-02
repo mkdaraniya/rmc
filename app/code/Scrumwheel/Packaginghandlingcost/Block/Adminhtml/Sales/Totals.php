1ba61df8f30f27331dd29f75de3dbbcd62190be5
lalit <lalitvadher18031996@gmail.com>
2023-02-02 20:56:38 +0530

    module has been added

--- app/code/Scrumwheel/Packaginghandlingcost/Block/Adminhtml/Sales/Totals.php
+++ app/code/Scrumwheel/Packaginghandlingcost/Block/Adminhtml/Sales/Totals.php
@@ -75,7 +75,7 @@ class Totals extends \Magento\Framework\View\Element\Template
             ]
         );
         $this->getParentBlock()->addTotalBefore($total, 'grand_total');
-
+		
         return $this;
     }
 }
