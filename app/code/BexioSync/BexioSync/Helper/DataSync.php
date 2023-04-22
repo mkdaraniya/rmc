70f29ea31cc881abf2ae5ad92ecd9ebb2e351c0b
Milind daraniya <mkdaraniya@gmail.com>
2023-04-22 15:35:56 +0530

    customer observer has been added in bexio module

---
+++ app/code/BexioSync/BexioSync/Helper/DataSync.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BexioSync\BexioSync\Helper;
+
+use \Magento\Framework\App\Helper\AbstractHelper;
+
+class DataSync extends AbstractHelper
+{
+    public function createUser($data)
+    {
+        
+    }
+}
